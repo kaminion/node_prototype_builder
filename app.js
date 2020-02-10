8e7db949f0241d592d60a30933ff8759f5debdb4
kaminion <anysweetimes@naver.com>
2020-02-10 19:28:52 +0900

    proto directory

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ app.set("view engine", "pug"); // 뷰엔진설정
 app.set("views", "./views"); // 뷰엔진 디렉터리설정
 
 app.use("/", express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({ extenede: false }));
 
 
 app.get("/", (req, res) => {
-    res.send("asda" + process.env.domainName);
 
+    res.render("./index.pug");
 })
 
 
